d9859eeea94434d736c8c14c92c52a28d8837133
anuragwork12 <anuragchaubey2002@gmail.com>
2025-03-12 10:52:01 +0530

    Created Palindrome check programme

--- palindrome.js
+++ palindrome.js
@@ -1,2 +1,13 @@
-var a=10
-console.log(a)
+//palindrome check
+const isPalindrome =(str)=>{
+    let reverse ="";
+    for(let char = str.length-1; char>=0; char--){
+        reverse = reverse + str[char];
+    }
+    return str==reverse?true:false;
+};
+
+console.log(isPalindrome("level")); 
+// git add .
+// git commit -m'bdjfrebi'
+// git push -u origin main
