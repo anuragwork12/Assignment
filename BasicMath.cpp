2496ce06eeb77799b93e1e5747c15c36f389bd3f
anuragwork12 <anuragchaubey2002@gmail.com>
2025-03-16 21:05:46 +0530

    Question

--- BasicMath.cpp
+++ BasicMath.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 
 //REVERSE A NUMBER  
-// int main(){
-//     int n;
-//     cin>>n;
-//     int revN=0;
-//     while(n>0){
-//         int ld=n%10;
-//         revN=(revN*10)+ld;
-//         n=n/10;
-//     }
-//     cout<<revN;
-// }
+int main(){
+    int n;
+    cin>>n;
+    int revN=0;
+    while(n>0){
+        int ld=n%10;
+        revN=(revN*10)+ld;
+        n=n/10;
+    }
+    cout<<revN;
+}
 
 
 //CHECK PALINDROME
